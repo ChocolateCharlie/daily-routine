a03499d368264b4cc7c4de572cdd67b152e05cbb
ChocolateCharlie <aurore.amrit@gmail.com>
2021-08-31 19:38:09 +0200

    Creates read.h
    Adds dirty readfile function

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/* --------------------------------------------------------------------------*/
+/*                                                             DAILY ROUTINE */
+/* main.cpp                                                                  */
+/* By: ChocolateCharlie                                                      */
+/* Updated: 2021-08-31                                                       */
+/* ------------------------------------------------------------------------- */
+#include <iostream>
+#include "Link.h"
+#include "read.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	readfile();
+	return(0);
+}
+
---
+++ read.h
@@ -0,0 +1,16 @@
+/* --------------------------------------------------------------------------*/
+/*                                                             DAILY ROUTINE */
+/* read.h                                                                    */
+/* By: ChocolateCharlie                                                      */
+/* Updated: 2021-08-31                                                       */
+/* ------------------------------------------------------------------------- */
+#ifndef READ_H_INCLUDED
+#define READ_H_INCLUDED
+
+#include <fstream>
+#include <string>
+
+void readfile();
+
+# endif // READ_H_INCLUDED
+
